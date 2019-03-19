19414f4865a07ef5403c27ab8a5fe734b73d641a
JayRodrig <mrjosemlrod@gmail.com>
2019-03-19 01:55:36 -0400

    added two private user routes, have to write tests for them

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const bodyParser = require('body-parser');
 
 // ROUTER MODULES   
 const PublicUserRouter = require('./routes/public_user_router');
+const PrivateUserRouter = require('./routes/private_user_router');
 
 // MIDDLEWARE
 app.use(bodyParser.json());
 app.use('/user', PublicUserRouter);
+app.use('/user', PrivateUserRouter);
 
 // EXPRESS ROUTES
 app.get('/', (request, response) => {
