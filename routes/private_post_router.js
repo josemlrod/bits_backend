972e05fbf56707f9b6325b376812c4b78dc8246d
JayRodrig <mrjosemlrod@gmail.com>
2019-03-20 01:15:22 -0400

    finished private post routes, will write tests now

--- routes/private_post_router.js
+++ routes/private_post_router.js
@@ -18,7 +18,7 @@ PrivatePostRouter.post('/', (request, response) => {
             .then(() => {
                 response.status(200);
                 response.json({
-                    'msg': `Success`,
+                    'msg': `Successfully created post.`,
                 });
             })
             .catch(err => {
@@ -26,6 +26,54 @@ PrivatePostRouter.post('/', (request, response) => {
                 response.status(400);
                 response.json({
                     'msg': `err, Something went wrong.`,
+                });
+            });
+    }
+});
+
+PrivatePostRouter.put('/:post_id', (request, response) => {
+    const {post_id} = request.params;
+    if (!post_id || typeof parseInt(post_id) !== 'number') {
+        response.status(400);
+        response.json({
+            'msg': `err. Something went wrong.`,
+        });
+    } else {
+        PostServices.updatePost(post_author, post_img, post_text)
+            .then(() => {
+                response.status(200);
+                response.json({
+                    'msg': `Successfully updated post.`,
+                });
+            })
+            .catch(err => {
+                response.status(400);
+                response.jsonp({
+                    'msg': `err. Something went wrong.`,
+                });
+            });
+    }
+});
+
+PrivatePostRouter.delete('/:post_id', (request, response) => {
+    const {post_id} = request.params;
+    if (!post_id || typeof parseInt(post_id) !== 'number') {
+        response.status(400);
+        response.json({
+            'msg': `err. Something went wrong.`,
+        });
+    } else {
+        PostServices.deletePost(post_id)
+            .then(() => {
+                response.status(200);
+                response.json({
+                    'msg': `Successfully deleted post.`,
+                });
+            })
+            .catch(err => {
+                response.status(400);
+                response.json({
+                    'msg': `err. Something went wrong.`,
                 });
             });
     }
---
+++ tests/private_post_router.test.js
@@ -0,0 +1,9 @@
+// const request = require('supertest');
+
+// jest.mock('../services/post_services.js');
+// const PostServices = require('../services/post_services');
+
+// jest.mock('../services/database.js');
+// const db = require('../services/database');
+
+// test('')
