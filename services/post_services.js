86246f3c6d0370c0c9cbb75e06077ff330bfc7aa
JayRodrig <mrjosemlrod@gmail.com>
2019-04-12 01:42:06 -0400

    ...

--- services/post_services.js
+++ services/post_services.js
@@ -19,8 +19,7 @@ PostServices.readPost = post_id => db.one(
 );
 
 PostServices.readPostComments = post_id => db.any(
-    `
-    SELECT 
+    `SELECT 
        *
     FROM posts JOIN comments 
         ON 
@@ -28,9 +27,6 @@ PostServices.readPostComments = post_id => db.any(
         JOIN users 
         ON
     posts.post_author = users.id
-    	JOIN likes
-    	ON
-    likes.post_liked = posts.id
     WHERE comments.post_id = $[post_id]
     `, {post_id,}
 );
